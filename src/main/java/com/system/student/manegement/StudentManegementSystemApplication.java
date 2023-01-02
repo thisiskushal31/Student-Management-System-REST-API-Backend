8a71f7987d70447e23da2acce00f7c0d6939211c
thisiskushal31 <guptakushal131@gmail.com>
2023-01-02 22:38:00 +0530

    Maven Project Intial Boast

---
+++ src/main/java/com/system/student/manegement/StudentManegementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.system.student.manegement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentManegementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentManegementSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/system/student/manegement/StudentManegementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.system.student.manegement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentManegementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
